3e2f6357249bb607c017de4cdd42e78428f30b55
trinathkella <148771994+trinathkella@users.noreply.github.com>
2024-11-29 22:23:11 +0530

    comparator.sv

---
+++ Combinational_Circuits/comparator.sv
@@ -0,0 +1,12 @@
+module onebit_comparator(
+    input a_in,
+    input b_in,
+    output g_out,
+    output e_out,
+    output l_out
+    );
+    //Three Conditions Less than | Greater Than | Equal to
+    assign g_out = a_in & (~b_in);
+    assign l_out = (~a_in) & b_in;
+    assign e_out = ~(a_in ^ b_in);
+endmodule
