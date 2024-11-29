b290db9fdc968f93eb2e3a4eb78c030e4652ff5d
trinathkella <148771994+trinathkella@users.noreply.github.com>
2024-11-29 22:32:37 +0530

    Flipflops_Design.v

---
+++ Sequential_Circuits/Flipflops_Design.v
@@ -0,0 +1,22 @@
+module flipflops(
+    input a_in,
+    input b_in,
+    input clk,
+    input reset_n,
+    output reg q_out
+    );
+    reg q1,q2;
+    always @ (posedge clk, negedge reset_n)
+    begin
+        if(!reset_n) begin
+            q1 <= 1'b0;
+            q2 <= 1'b0;
+            q_out <= 1'b0;
+        end
+        else begin
+            q1 <= a_in;
+            q2 <= b_in;
+            q_out <= !(q1&q2);
+        end
+    end
+endmodule
