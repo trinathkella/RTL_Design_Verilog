aca5285f09018e6a55b74050c45dcdbe211e3d5a
trinathkella <148771994+trinathkella@users.noreply.github.com>
2024-11-29 22:25:10 +0530

    Addition_Subtraction.v

--- Combinational_Circuits/Addition_Subtraction.v
+++ Combinational_Circuits/Addition_Subtraction.v
@@ -2,7 +2,7 @@ module nbit_add_sub #(parameter N = 4)(
     input [N-1:0]a_in,
     input [N-1:0]b_in,
     input opcode,
-    output reg [N-1:0]sum_or_diff_out,
+    output reg [N-1:0]sum_or_diff_out;
     output reg carry_or_borrow_out
     );
     reg [N-1:0]temp_out;
